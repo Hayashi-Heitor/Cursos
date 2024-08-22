621c460a6334aa1bc364054a224105768a9351c2
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-08-22 16:54:28 -0300

    Create Aula(1)-OperaçõesMatemáticas.py

---
+++ Aula(1)-OperaçõesMatemáticas.py
@@ -0,0 +1,30 @@
+numero1 = int(input("Informe o primeiro número: "))
+numero2 = int(input("Informe o segundo número: "))
+
+# Soma
+soma = numero1 + numero2
+print(f"\nSoma dos números: {soma}")
+
+# Subtração
+sub = numero1 - numero2
+print(f"Sutração dos números: {sub}")
+
+# Multiplicação
+multi = numero1 * numero2
+print(f"Multiplicação dos números: {multi}")
+
+# Divisão
+div = numero1 / numero2
+print(f"Divisão dos números: {div}")
+
+# Divisão inteira (passa o resultado para int)
+div_i = numero1 // numero2
+print(f"Divisão inteira dos números: {div_i}")
+
+# Potência
+pot = numero1 ** numero2
+print(f"Potência dos números: {pot}")
+
+# Módulo (Resto da divisão)
+mod = numero1 % numero2
+print(f"Resto da divisão dos números: {mod}")
