5ef3cab6ddfdbbe5b3ab4c61ef66b27b8503d0a7
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-08-27 13:31:39 -0300

    Create Aula(2)-Operadores_Lógicos.py

---
+++ Python-Senai/Aula(2)-Operadores_Lógicos.py
@@ -0,0 +1,18 @@
+a = 1
+b = 5
+c = 4
+d = 1
+
+print(a == d)
+print(c >= b)
+print(d <= a)
+
+x = 5
+y = 5
+print("\n", x == y)
+print(x is y)
+print(id(x))  # id = endereço de memória
+print(id(y))
+print("\n", a is b)
+print(id(a))
+print(id(b))
