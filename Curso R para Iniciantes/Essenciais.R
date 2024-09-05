d8133ed6b3a98cd3c2c0004d565fcbee9e683b1f
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-09-05 18:03:38 -0300

    Add files via upload

---
+++ Curso R para Iniciantes/Essenciais.R
@@ -0,0 +1,23 @@
+a <- 35 
+b <- "casa" 
+rm(b) #remove
+
+#função combine (cria conjuntos)
+combine <- c(3, 18, 27)
+frutas <- c("Abacaxi", "Morango")
+
+#sequencia de números
+centena <- 1:100
+
+#a soma na variável é aplicada para todos os valores
+valores <- c(2, 6, 5, 3)
+valores = valores + 2
+
+#funções estatísticas básicas
+mean(valores) #média
+median(valores) #mediana
+min(valores) #menor valor
+max(valores) #maior valor
+summary(valores) #todas as informações anteriores
+
+paste("Eu gosto muito de", frutas, sep=" ")#junta strings
